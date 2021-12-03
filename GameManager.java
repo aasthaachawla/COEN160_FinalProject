ffcd434d04f54a7069e07ff4b5787fb342aa72d0
aastha <aastha@ac-star.local>
2021-12-02 22:53:24 -0800

    Updated GUI colors and smaller bugs

--- GameManager.java
+++ GameManager.java
@@ -23,13 +23,17 @@ public class GameManager {
     private JPanel playerInfoPanel;
     private JPanel wordPanel;
     private JPanel interactiveGamePanel;
+    private JPanel timerAndWordPanel;
+    private JPanel descriptionPanel;
 
     private JTextField nameField;
     private JTextField currentScoreField;
     private JTextField highScoreField;
+    private JButton gameDescription;
+    
     private JTextField timerLabel;
-
     private JTextField givenScrambleTextField;
+
     private JTextArea wordsInputtedTextArea;
     private JTextField resultTextField;
 
@@ -68,18 +72,22 @@ public class GameManager {
         keys.add("copiduce"); // occupied
         keys.add("bacgikn");
         keys.add("daginot");
-        // keys.add("DAGINOT");
-        // keys.add("DAGINOT");
+        keys.add("ipantca");
+        keys.add("dsismis");
+        keys.add("alpidpe");
+        keys.add("nahmopt");
+        keys.add("ibgorty");
     }
 
     /* Starts the GUI and builds a User/Player object. Sets all necessary attributes.*/
     public void startGame(){
         buildGUI();
         // new User, send a JOptionPane to create this user.
-        String getUserInput = JOptionPane.showInputDialog(topPanel, "Enter your name.");
+        String getUserInput = JOptionPane.showInputDialog(topPanel, "Enter your name to login as an existing user \nor create a new user. Select cancel to login as a guest.");
         if(getUserInput == null){
             nameField.setText("Name: Guest");
             currentScoreField.setText("Current Score: 0");
+            player = new User("Guest", 0, 0, "Beginner");
         }
         else {
             player = fm.checkIFUserExists(getUserInput);
@@ -88,7 +96,10 @@ public class GameManager {
             player.setCurrScore(0);
             currentScoreField.setText("Current Score: 0");
         }
+        
         givenScrambleTextField.setText(currWord);
+        howToPlay();
+        givenScrambleTextField.setForeground(Color.black);
         setTimer();
     }
 
@@ -98,9 +109,10 @@ public class GameManager {
 		int n = JOptionPane.showOptionDialog(window, "Game over, thanks for playing! Do you want to play again?", "Game over!", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
 		System.out.println(n);
         if(n == 1) {
-            // update the stored object information on this user, serialize it.
+            // update the stored object information on this user provided they are not a guest, and serialize it.
             player.setHighScore(maxScore());
-            fm.updateUserObject(player);
+            if(!player.getName().equals("Guest"))
+                fm.updateUserObject(player);
             window.dispose();
         }
         else {
@@ -136,46 +148,68 @@ public class GameManager {
         // instantiate JPanels and JFrame components 
         window = new JFrame();
 		topPanel = new JPanel(new BorderLayout());
-        playerInfoPanel = new JPanel(new GridLayout(0, 4));
+        playerInfoPanel = new JPanel(new GridLayout(0, 3));
         wordPanel = new JPanel(new BorderLayout());
         interactiveGamePanel = new JPanel(new BorderLayout());
+        timerAndWordPanel = new JPanel(new GridLayout(2, 0));
+        descriptionPanel = new JPanel(new BorderLayout());
 
-        // create GUI components for the playerInfoPanel - name, score, and timer
+        // create GUI components for the playerInfoPanel - name, score, and description
         nameField = new JTextField("Name: " + "Guest");
         nameField.setEditable(false);
         nameField.setHorizontalAlignment(JTextField.CENTER);
+        nameField.setBackground(Color.decode("#CCDCFD"));
         currentScoreField = new JTextField("Current Score: " + "0");
         currentScoreField.setEditable(false);
         currentScoreField.setHorizontalAlignment(JTextField.CENTER);
+        currentScoreField.setBackground(Color.decode("#CCDCFD"));
         highScoreField = new JTextField("High Score: " + "0");
         highScoreField.setEditable(false);
         highScoreField.setHorizontalAlignment(JTextField.CENTER);
+        highScoreField.setBackground(Color.decode("#CCDCFD"));
+        playerInfoPanel.add(nameField);
+        playerInfoPanel.add(currentScoreField);
+        playerInfoPanel.add(highScoreField);
+        playerInfoPanel.setBackground(Color.decode("#9EA3F5"));
+        
         timerLabel = new JTextField("");
         timerLabel.setEditable(false);
         timerLabel.setHorizontalAlignment(JTextField.CENTER);
-        playerInfoPanel.add(nameField);
-        playerInfoPanel.add(timerLabel);
-        playerInfoPanel.add(highScoreField);
-        playerInfoPanel.add(currentScoreField);
+        timerLabel.setBackground(Color.decode("#CCDCFD"));
+        timerLabel.setForeground(Color.black);
+        timerAndWordPanel.add(timerLabel);
+        timerAndWordPanel.add(playerInfoPanel);
+        timerAndWordPanel.setBackground(Color.decode("#9EA3F5"));
 
-        // create GUI components for wordPanel - valid user inputted words and the result status
+        // create GUI components for wordPanel - timer, valid user inputted words and the result status
         givenScrambleTextField = new JTextField(currWord); // TODO: autogenerate these words
         givenScrambleTextField.setEditable(false);
+        givenScrambleTextField.setHorizontalAlignment(JTextField.CENTER);
+        givenScrambleTextField.setForeground(Color.decode("#BBBEFE"));
+        givenScrambleTextField.setBackground(Color.decode("#BBBEFE"));
         wordsInputtedTextArea = new JTextArea(200, 400);
         wordsInputtedTextArea.setEditable(false);
+        wordsInputtedTextArea.setBackground(Color.decode("#CCDCFD"));
         resultTextField = new JTextField();
         resultTextField.setEditable(false);
+        resultTextField.setBackground(Color.decode("#BBBEFE"));
         wordPanel.add(givenScrambleTextField, BorderLayout.NORTH);
         wordPanel.add(wordsInputtedTextArea, BorderLayout.CENTER);
         wordPanel.add(resultTextField, BorderLayout.SOUTH);
+        wordPanel.setBackground(Color.decode("#9EA3F5"));
 
         // create GUI components for interactiveGamePanel - buttons and text field for user to interact with
+        gameDescription = new JButton("ⓘ");
         refreshGame = new JButton("Refresh Game");
+        descriptionPanel.add(gameDescription, BorderLayout.WEST);
+        descriptionPanel.add(refreshGame, BorderLayout.EAST);
+        descriptionPanel.setBackground(Color.decode("#9EA3F5"));
         userInputField = new JTextField("", 100);
         submitWord = new JButton("Submit");
-        interactiveGamePanel.add(refreshGame, BorderLayout.WEST);
+        interactiveGamePanel.add(descriptionPanel, BorderLayout.WEST);
         interactiveGamePanel.add(userInputField, BorderLayout.CENTER);
         interactiveGamePanel.add(submitWord, BorderLayout.EAST);
+        interactiveGamePanel.setBackground(Color.decode("#9EA3F5"));
 
         // add action listener to user input text field
         userInputField.addActionListener(new ActionListener() {
@@ -199,12 +233,19 @@ public class GameManager {
             }
         });
 
+        gameDescription.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // give a pop up description
+                howToPlay();
+            }
+        });
+
         // add the main 3 panels to the top panel with a Border Layout 
         topPanel.setForeground(Color.white);
-        topPanel.add(playerInfoPanel, BorderLayout.NORTH);
+        topPanel.add(timerAndWordPanel, BorderLayout.NORTH);
         topPanel.add(wordPanel, BorderLayout.CENTER);
         topPanel.add(interactiveGamePanel, BorderLayout.SOUTH);
-
+        topPanel.setBackground(Color.lightGray);
         // set the window's content pane to the top panel and display the window
         window.setContentPane(topPanel);
         window.pack();
@@ -212,6 +253,11 @@ public class GameManager {
         window.setSize(700, 400);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setVisible(true);
+    }
+
+    /* show how to play before game */
+    public void howToPlay() {
+        JOptionPane.showMessageDialog(window, "You have 60 seconds to list as many words that can be created \nwith the letters you are given (and repeated letters are allowed). \nIn this time, you can press the “Refresh Board” button to change the set of letters. If the \n“Refresh Board” button is pressed 3 times, you will lose 5 points. \nIf you choose to login, you must play through a full game in order for your score to be stored. \nRules: \n- You must create words that are at least 3 letters in length, and you must provide unique words \n- any reused words will not increment points. \n- Points are assigned based on the number of letters in the word.", "Rules", JOptionPane.INFORMATION_MESSAGE);
     }
 
     /* update the score and word-input area when the user inputs a word */
@@ -243,6 +289,8 @@ public class GameManager {
 
     /* update the word on the board */
     public void updateWord(){
+        if(keys.isEmpty())
+            initializeKeys();
         // remove current word on board
         keys.remove(currWord);
         // remove letters from validation set
@@ -258,7 +306,6 @@ public class GameManager {
     public void resetBoardEndGame() {
         userInputField.setText("");
         wordsInputtedTextArea.setText("");
-        resultTextField.setText("");
 
         // set board to new word 
         // remove this key from the arraylist when its used 
@@ -270,14 +317,16 @@ public class GameManager {
         // update the board counter
         boardCounter++;
         // if the reset button has been hit 3 times, decrement the score by 5 and display the status
-        if(boardCounter == 3){
+        if(boardCounter >= 3){
             // score -= 5; // TODO: do we need this?
             // TODO: call updateUserScore in place of modifying here?
             player.setCurrScore(player.getCurrScore()-5);
             currentScoreField.setText("Current Score: " + player.getCurrScore());
-            
-            boardCounter = 0;
-            resultTextField.setText("Board has been reset 3 times! You lose 5 points.");
+            resultTextField.setForeground(Color.blue);
+            if(boardCounter == 3)
+                resultTextField.setText("Board has been reset 3 times! You lose 5 points.");
+            else 
+                resultTextField.setText("Board has been reset more than 3 times! You lose 5 points.");
         }
 
         resetBoardEndGame();   
